9120c8a61051942fa12b0f35f5f99a314f992b43
Badhon111 <31757434+Badhon111@users.noreply.github.com>
2017-09-18 00:41:28 +0600

    added

---
+++ php/syntex.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<title>Untitled Document</title>
+	</head>
+
+<body>
+	<?php
+		$a = 3;
+	$b = 1;
+		echo $a + $b;
+		?>
+</body>
+
+</html>
