c45ab17c9cb6b657aff67393849a76e76a1aae7d
Badhon111 <31757434+Badhon111@users.noreply.github.com>
2017-09-17 23:58:28 +0600

    style

--- index.php
+++ index.php
@@ -1,14 +1,18 @@
 <!doctype html>
 <html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
+	<head>
+	<meta charset="utf-8">
+	<title>Untitled Document</title>
+	</head>
 
 <body>
-<?php
-	$name = 10 + 11;
-	echo $name
-	?>
+	<?php
+		$name = 10 + 11;
+		echo $name
+		?>
 </body>
+
 </html>
+	<?php
+
+			?>
