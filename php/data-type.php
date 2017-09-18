faa8df1d504dd45aff2995532479a749861761cd
Badhon111 <31757434+Badhon111@users.noreply.github.com>
2017-09-18 13:06:19 +0600

    data type of "PHP" method" texting

---
+++ php/data-type.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+
+			<body>
+				
+				<?php
+				/* integer useing example */
+				$a = 15;
+				var_dump($a);
+				
+					echo('<br/>');
+				/* string using example */
+				$a = 'Badhon';
+				var_dump($a);
+				/* boolen using example */
+				echo('<br/>');
+				$a = true;
+				var_dump($a);
+				
+					echo('<br/>');
+				
+				
+				?>
+				
+				
+			</body>
+</html>
